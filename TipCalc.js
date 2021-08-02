fdb8e980791377924433c543de6bb850a50cead9
TestCode <68029426+FMcoding@users.noreply.github.com>
2021-08-02 19:25:56 -0400

    Add files via upload

---
+++ TipCalc.js
@@ -0,0 +1,10 @@
+/* $("#num-people").change(function() {
+    alert("The text has been changed.");
+    $("body").css("background", "blue");
+    $("button[type=reset]").prop("disabled", false);
+}) */
+
+
+$("button[type=reset]").click(function() {
+    alert("you clicked");
+})
